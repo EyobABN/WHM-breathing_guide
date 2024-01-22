af894cc70077a097d8ff24409cd3c866a1d18785
EyobABN <eyob.alamerew.belay@gmail.com>
2024-01-22 10:08:50 +0300

    Add footer

---
+++ src/App/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      2024 Eyob A Belay.&nbsp;
+      <a href="https://github.com/eyobabn" rel="noreferrer" target="_blank">GitHub</a>&nbsp;
+      <a href="https://linkedin.com/in/eyob-alamerew-belay" rel="noreferrer" target="_blank">LinkedIn</a>
+    </div>
+  );
+}
+
+export default Footer;
